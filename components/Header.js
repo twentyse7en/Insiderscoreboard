46eb881a9c24e2b2beb6ebb552f781f873c0d0f5
Abijith <abhil@keyvalue.systems>
2021-10-03 23:12:48 +0530

    basic layout

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faClipboard } from '@fortawesome/free-solid-svg-icons'
+
+export default function Header() {
+	return (
+		<header className="bg-gradient-to-r from-purple-700 to-[#a65fec] text-center text-white bg-blue-800 p-8 mb-16">
+			<h1 className="text-2xl md:text-4xl font-bold">
+				{/*
+				<FontAwesomeIcon icon={faClipboard} />
+				*/}
+				Insider Scoreboard
+			</h1>
+		</header>
+	);
+}
---
+++ components/layout.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+
+import Header from './Header';
+import Footer from './Footer';
+
+
+export default function Layout({ children }) {
+	return (
+		<div className="flex flex-col min-h-screen">
+			<Head>
+				<title>my next app</title>
+				{/* Add more tags */}
+				<meta name="keywords" content=""/>
+			</Head>
+
+			<Header />
+			<main className="flex-1 lg:container px-4 py-6 mx-auto md:px-6 md:py-12">
+				{children}
+			</main>
+			<Footer />
+		</div>
+	);
+}
